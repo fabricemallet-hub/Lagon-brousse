c568b40d049323e03b3b9c06ad5d2688b6cc7c0c
Fabrice Mallet <Fabrice.MALLET@gmail.com>
2026-01-31 14:44:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2943478321-f746e",
+  "appId": "1:679064713235:web:93b38bd7feda744b24a7e6",
+  "apiKey": "AIzaSyDs6qQO274Ro2RD4lVkr8KztsZIecP-ZDk",
+  "authDomain": "studio-2943478321-f746e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "679064713235"
+};
