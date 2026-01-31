e9c5eafaebc1dfe967cc9aaa78a20ea0b28f7a26
Fabrice Mallet <Fabrice.MALLET@gmail.com>
2026-01-31 16:20:20 +0000

    trouve moi un logo pour l'application qui sera l'identification dans pla

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,11 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      {/* This is the nautilus/wave part representing the "Lagon" */}
       <path d="M12 22a7 7 0 0 0 7-7c0-2-1-3.9-3-5.5s-3.5-2.5-5.5-3.5a7 7 0 0 0-7 7c0 2 1 3.9 3 5.5s3.5 2.5 5.5 3.5z" />
       <path d="M2 13.2A12 12 0 0 0 12 22a12 12 0 0 0 10-8.8" />
-      <path d="M2 10.5V6.2c0-1.2.9-2.2 2.2-2.2h1.3" />
+      {/* This is a new part, a stylized mountain for the "Brousse" */}
+      <path d="M5 10l4-4 4 4" />
     </svg>
   );
 }
