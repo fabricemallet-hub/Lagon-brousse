ac0ec207bbe80f645eb88fa19e23121286a49d10
Fabrice Mallet <Fabrice.MALLET@gmail.com>
2026-02-02 11:09:11 +0000

    22:03:30.435 firebase-studio-1769852895244.cluster-bqwaigqtxbeautecnatk4

--- next.config.ts
+++ next.config.ts
@@ -49,6 +49,14 @@ const nextConfig: NextConfig = {
             key: "Access-Control-Allow-Origin", 
             value: "*" 
           },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type"
+          }
         ],
       },
     ];
