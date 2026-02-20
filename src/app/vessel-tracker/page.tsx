9becc1746f5056064b016dc2b56a5c8beb3c0ecf
Fabrice Mallet <Fabrice.MALLET@gmail.com>
2026-02-20 05:05:33 +0000

    Try fixing this error: `Runtime ReferenceError: Select is not defined. E

--- src/app/vessel-tracker/page.tsx
+++ src/app/vessel-tracker/page.tsx
@@ -9,6 +9,13 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { useToast } from '@/hooks/use-toast';
 import Script from 'next/script';
 import { 
